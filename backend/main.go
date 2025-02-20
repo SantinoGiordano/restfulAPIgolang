78309cbe3900195ea2081a0ffdd11dc5d4e8f2fc
SantinoGiordano <santinogiordano13@gmail.com>
2025-02-20 11:15:33 -0500

    added struct

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,13 @@
 package main
 
-
 import "fmt"
 
-
-func main(){
+func main() {
+	type album struct {
+		ID     string  `json:"id"`
+		Title  string  `json:"title"`
+		Artist string  `json:"artist"`
+		Price  float64 `json:"price"`
+	}
 	fmt.Print("hello world")
 }
