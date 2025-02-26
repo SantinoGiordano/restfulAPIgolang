4627c97018f625174a3123a7d7d7366e86037649
SantinoGiordano <santinogiordano13@gmail.com>
2025-02-25 21:47:02 -0500

    Added typing inteface and simple try catch

--- frontend/album-project/src/App.tsx
+++ frontend/album-project/src/App.tsx
@@ -1,26 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+
+interface Album {
+  id: number;
+  title: string;
+  
+}
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState<Album[]>([]);
 
   useEffect(() => {
-    
     const fetchData = async () => {
       try {
-        
+        const response = await fetch("http://localhost:8080/albums");
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setMessage(data); // Assuming the API returns an array of Album objects
       } catch (error) {
-        
+        console.error('Error fetching data:', error);
       }
-    }
+    };
+    fetchData()
   }, []);
 
   return (
-      <div>
-        {message.map((item) => (
-        <li key={item.id}>{item.name}</li>
+    <div>
+      {message.map((item) => (
+        <li key={item.id}>{item.title}</li>
       ))}
-      </div>
-    
+    </div>
   );
 }
 
